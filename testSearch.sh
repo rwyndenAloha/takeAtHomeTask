8b1dc420de880e5ef96b9a656049abf76396de2f
Joja Poppa <jojapoppa@protonmail.com>
2025-05-31 14:10:23 -0700

    updates for id

--- testSearch.sh
+++ testSearch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-curl -X POST "http://10.10.10.129:8000/libraries/f3f08fd0-6f7a-4946-8338-aba7a9f8e239/search/" \
+curl -X POST "http://10.10.10.129:8000/libraries/ba75b96a-7dcb-4800-bbb8-d69907e67198/search/" \
      -H "Content-Type: application/json" \
      -d '{"embedding": [0.1, 0.2, 0.3], "k": [2]}'
--- testUpdateLibraryMetadata.sh
+++ testUpdateLibraryMetadata.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-library_id="f3f08fd0-6f7a-4946-8338-aba7a9f8e239"
+library_id="ba75b96a-7dcb-4800-bbb8-d69907e67198"
 curl -X PUT "http://10.10.10.129:8000/libraries/$library_id" \
      -H "Content-Type: application/json" \
      -d '{"metadata": {"updated": "true", "category": "test"}}'
